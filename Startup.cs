e74a3361e03026a9cc0232025c00bf0536496fea
Vladimir <nicon-83@mail.ru>
2019-10-27 23:29:59 +0500

    Add CORS

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace MyWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
             services.AddEntityFrameworkNpgsql();
         }
@@ -36,6 +37,14 @@ namespace MyWebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder =>
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+            );
 
             app.UseMvc();
         }
